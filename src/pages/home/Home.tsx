810175128ef238523b85aa0c4d36731f5477cc80
Hamza <hamza@mindbehind.com>
2022-08-15 02:56:07 +0300

    Getting mobile dimension

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   CreditCardList,
   Transactions,
   WelcomeComponent,
 } from "../../components";
+import { MOBILE_DIMENSION } from "../../utilities/utils";
+import useWindowSize from "../../utilities/windowResize/windowResize";
 
 //mobile view width less tha 950
 
 function Home() {
+  const [pageWidth] = useWindowSize();
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (pageWidth < MOBILE_DIMENSION) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }, [pageWidth]);
+
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <div className="homeOuterContainer">
-        <WelcomeComponent isMobile={false} />
+        <WelcomeComponent isMobile={isMobile} />
         <div className="homeInnerContainer">
-          <CreditCardList isMobile={false} />
+          <CreditCardList isMobile={isMobile} />
           <Transactions />
         </div>
       </div>
